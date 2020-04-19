fbb9139d206c6a5a57763b0e148a76d87049901d
Antonio Espinoza <aespinoza777@gmail.com>
2020-04-19 18:55:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(int)
 
   while int > 0
-  puts "#{{int} SECONDS(S)!}"
+  puts "#{int} SECONDS(S)!}"
   int -=1
 end
 "Happy New Year!"
@@ -11,5 +11,9 @@ end
 
 def countdown_with_sleep(int)
   while int > 0
-
-countdown_with_sleep(10)
+puts "#{int} SECONDS(S)!"
+int -= 1
+sleep 1
+end
+"HAPPY NEW YEAR!"
+end
